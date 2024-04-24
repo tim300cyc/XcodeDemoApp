6dc618a79452672702b912128ba8ee16fce74e0d
Tim <tim300cyc@gmail.com>
2024-04-24 14:48:03 +0800

    Initial Commit

---
+++ XcodeDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeDemoApp
+//
+//  Created by PrickupDriver on 2024/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeDemoApp/XcodeDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoAppApp.swift
+//  XcodeDemoApp
+//
+//  Created by PrickupDriver on 2024/4/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
